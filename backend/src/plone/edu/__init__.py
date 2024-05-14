b743677d317bdd568dc121297ee944d8757afa7a
Adrian Schulz <adrian.schulz@fhnw.ch>
2024-05-14 15:37:46 +0200

    Added textindexer for person (first_name, last_name, description)

--- backend/src/plone/edu/__init__.py
+++ backend/src/plone/edu/__init__.py
@@ -1,6 +1,9 @@
 """Init and utils."""
 
+from collective.person.behaviors.person import IPersonData
+from plone.app.dexterity.textindexer import utils
 from zope.i18nmessageid import MessageFactory
+
 
 import logging
 
@@ -8,3 +11,7 @@ import logging
 _ = MessageFactory("plone.edu")
 
 logger = logging.getLogger("plone.edu")
+
+utils.searchable(IPersonData, "first_name")
+utils.searchable(IPersonData, "last_name")
+utils.searchable(IPersonData, "description")
